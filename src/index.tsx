c1a2a7495f1f048eb9f2de555aeafd52deb9a602
Evan Rosson <git@erosson.org>
2023-01-13 05:25:09 +0000

    docs and style

--- src/index.tsx
+++ src/index.tsx
@@ -4,27 +4,9 @@ import "./main.css";
 import * as T9 from "./t9";
 import rawWordsUrl from "./words.txt";
 
-const style = {
-  button: { flex: 1, padding: "2em" },
-  buttonRow: { display: "flex", flex: 1 },
-  entrySelected: { backgroundColor: "yellow" },
-  entryUnselected: {},
-  cursor: {
-    // https://www.amitmerchant.com/simple-blinking-cursor-animation-using-css/
-    backgroundColor: "black",
-    width: "0.5em",
-    height: "1em",
-    display: "inline-block",
-    animation: "cursor-blink 1.5s steps(2) infinite",
-  },
-};
-
-async function fetchDictionary() {
-  const response = await fetch(rawWordsUrl);
-  const text = await response.text();
-  return T9.indexFromWords(text.split("\n"));
-}
-
+/**
+ * Application entry point. Load the dictionary, then display the input.
+ */
 function App(): JSX.Element {
   const [index, setIndex] = React.useState<T9.Index | null>(null);
   const [error, setError] = React.useState<string | null>(null);
@@ -41,6 +23,9 @@ function App(): JSX.Element {
   );
 }
 
+/**
+ * App UI and state after our dictionary has been loaded + processed
+ */
 function Ready(props: { index: T9.Index }): JSX.Element {
   const [text, setText] = React.useState<string>("");
   const [nextDigits, setNextDigits] = React.useState<number[]>([]);
@@ -86,46 +71,20 @@ function Ready(props: { index: T9.Index }): JSX.Element {
     }
   }
   return (
-    <div
-      style={{
-        border: "1px solid gray",
-        height: "100vh",
-        display: "flex",
-        flexFlow: "column",
-        boxSizing: "border-box",
-      }}
-    >
+    <div style={style.root}>
       <div style={{ flex: 1, overflow: "auto" }}>
         <div style={{ padding: "1em" }}>
           <span style={{ whiteSpace: "pre-wrap" }}>{text}</span>{" "}
           <u>
-            <ol
-              style={{
-                display: "inline-block",
-                position: "absolute",
-                padding: 0,
-                paddingTop: "1em",
-              }}
-            >
-              {nextStrings.map((w, i) => (
-                <li key={w}>
-                  <span
-                    style={
-                      i === nextIndex
-                        ? style.entrySelected
-                        : style.entryUnselected
-                    }
-                  >
-                    {w}
-                  </span>
-                </li>
-              ))}
-            </ol>
+            <WordList
+              words={nextStrings}
+              selected={nextIndex}
+              style={style.wordList}
+            />
             {nextString}
           </u>
           <span style={style.cursor} />
         </div>
-        {/* <div>{JSON.stringify(props.index[nextWord] ?? null)}</div> */}
         {text === "" && nextString === "" ? (
           <div style={{ color: "gray", padding: "1em" }}>
             Enter some text using the T9 keyboard below...
@@ -146,15 +105,106 @@ function Ready(props: { index: T9.Index }): JSX.Element {
   );
 }
 
-const digitGrid: Digit[][] = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  ["*", 0, "#"],
-];
+/**
+ * The list of words you're choosing from, with a single word selected
+ */
+function WordList(props: {
+  words: string[];
+  selected: number;
+  style: React.CSSProperties;
+}): JSX.Element {
+  return (
+    <ol style={props.style}>
+      {props.words.map((w, i) => (
+        <li key={w}>
+          <span
+            style={
+              i === props.selected ? style.entrySelected : style.entryUnselected
+            }
+          >
+            {w}
+          </span>
+        </li>
+      ))}
+    </ol>
+  );
+}
 
-type Digit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | "#" | "*";
+/**
+ * One of the input buttons at the bottom of the screen
+ */
+function Button(props: { digit: Digit; onClick: () => void }): JSX.Element {
+  return (
+    <button style={style.button} onClick={props.onClick}>
+      <h3>{digitToLabel(props.digit)}</h3>
+      <div>{props.digit}</div>
+    </button>
+  );
+}
 
+/**
+ * for TS apps, I like using a table of styles instead of css classes.
+ * better autocomplete; programmable styles when needed
+ */
+const style: { [s: string]: React.CSSProperties } = {
+  root: {
+    border: "1px solid gray",
+    height: "100vh",
+    display: "flex",
+    flexFlow: "column",
+    boxSizing: "border-box",
+  },
+  wordList: {
+    display: "inline-block",
+    position: "absolute",
+    padding: 0,
+    paddingTop: "1em",
+  },
+  button: { flex: 1, padding: "2em" },
+  buttonRow: { display: "flex", flex: 1 },
+  entrySelected: { backgroundColor: "yellow" },
+  entryUnselected: {},
+  cursor: {
+    // https://www.amitmerchant.com/simple-blinking-cursor-animation-using-css/
+    backgroundColor: "black",
+    width: "0.5em",
+    height: "1em",
+    display: "inline-block",
+    animation: "cursor-blink 1.5s steps(2) infinite",
+  },
+};
+
+/**
+ * Fetch and index all dictionary words.
+ *
+ * `import './words.txt'` only gives us a url, not the whole file, so the app
+ * loads faster. We have to manually load that url, but it lets us show a
+ * "loading" screen (or maybe a partially working keyboard?!) instead of a
+ * blank screen while the app loads.
+ */
+async function fetchDictionary() {
+  const response = await fetch(rawWordsUrl);
+  const text = await response.text();
+  return T9.indexFromWords(text.split("\n"));
+}
+
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, "*", 0, "#"] as const;
+type Digit = (typeof digits)[number];
+// only those 12 digits are allowed. `const x: Digit = 10` will not typecheck
+
+/**
+ * The way digits are arranged in the UI
+ */
+const digitGrid: Digit[][] = digits.reduce((accum, digit, i) => {
+  const rownum = Math.floor(i / 3);
+  if (!accum[rownum]) accum.push([]);
+  accum[rownum].push(digit);
+  return accum;
+}, [] as Digit[][]);
+
+/**
+ * The text shown in the UI below each digit
+ */
 function digitToLabel(digit: Digit): string {
   switch (digit) {
     case "#":
@@ -168,15 +218,6 @@ function digitToLabel(digit: Digit): string {
     default:
       return T9.letters[digit].toUpperCase().split("").join(" ");
   }
-}
-
-function Button(props: { digit: Digit; onClick: () => void }): JSX.Element {
-  return (
-    <button style={style.button} onClick={props.onClick}>
-      <h3>{digitToLabel(props.digit)}</h3>
-      <div>{props.digit}</div>
-    </button>
-  );
 }
 
 function main() {
--- src/t9.ts
+++ src/t9.ts
@@ -1,13 +1,21 @@
-// a single digit of a T9 word. "h" => 4
+/**
+ * a single digit of a T9 word. example: `4` (representing "h")
+ */
 export type Digit = number;
 
-// a T9-encoded word; the user input. "hello" => "43556"
+/**
+ * a T9-encoded word; the user input. example: `"43556"` (representing "hello")
+ */
 export type Word = string;
 
-// given a T9-encoded string, find all possible dictionary words
+/**
+ * given a T9-encoded string, find all possible dictionary words for it
+ */
 export type Index = { [code: Word]: string[] };
 
-// build an index from a dictionary
+/**
+ * build an index from a list of dictionary words
+ */
 export function indexFromWords(words: string[]): Index {
   const index: Index = {};
   for (let word of words) {
@@ -22,7 +30,9 @@ export function indexFromWords(words: string[]): Index {
   return index;
 }
 
-// {2: "abc", 3: "def", ...}
+/**
+ * map digits to the letters each represents. `{2: "abc", 3: "def", ...}`
+ */
 export const letters = [
   "",
   "",
@@ -36,12 +46,17 @@ export const letters = [
   "wxyz",
 ];
 
-// inverted `letters` table. {a: 2, b: 2, c: 2, d: 3, e: 3, f: 3, ...}
+/**
+ * inverted `letters` table; map letters to their digits.
+ * `{a: 2, b: 2, c: 2, d: 3, e: 3, f: 3, ...}`
+ */
 export const byLetter: { [l: string]: number } = Object.fromEntries(
   letters.flatMap((ls, n) => ls.split("").map((l) => [l, n]))
 );
 
-// transform a string to a T9 word. "Hello!" => "43556"
+/**
+ * transform a string to a T9 word. `"Hello!"` => `"43556"`
+ */
 export function fromWord(word: string): Word {
   return word
     .toLowerCase()
